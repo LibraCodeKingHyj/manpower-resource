45aded312b018b158cbdce851fd499c87e2bb7bd
HYJ_coder <2628661255@qq.com>
2022-11-17 15:56:53 +0800

    base

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,18 @@
 module.exports = {
-
+  // 项目标题
   title: 'Vue Admin Template',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
+  // 固定头部
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
+  // 侧边栏logo
   sidebarLogo: false
 }
