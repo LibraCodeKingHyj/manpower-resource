16c89254a38e89026494748dfd7b5d747cb756c8
HYJ_coder <2628661255@qq.com>
2022-11-19 16:10:45 +0800

    vuex中新增token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,22 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
 
+const state = {
+  token: getToken() //设置token ,初始化vuex的时候，从缓存中读取token
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token //将数据设置给state里面的token
+    setToken(token)//将token同步给缓存
+  },
+  removeToken(state) {
+    state.token = null //将缓存置空
+    removeToken()//将token同步给缓存
+  }
+}
+const actions = {}
 export default {
   namespaced: true,
-  state: {
-    
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  mutations,
+  actions
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,14 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'hrsaas-ihrm-token'
 
 export function getToken() {
+  //获取token
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
+  //设置token
   return Cookies.set(TokenKey, token)
 }
 
