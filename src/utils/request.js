4d7a54a43e7f7230e36036a55f585107c0bc47b4
HYJ_coder <2628661255@qq.com>
2022-11-19 17:29:22 +0800

    配置request请求跟地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 // 创建axios实例
-const service = axios.create()
+const service = axios.create({
+  // 当执行 npm run dev => .env.development => /api => 跨域代理
+  baseURL: process.env.VUE_APP_BASE_API, //根据执行的命令选择是开发环境还是生产环境，选择不同的根地址
+  timeout: 5 * 1000
+})
 // 请求拦截器(成功和失败的回调函数)
 service.interceptors.request.use(
   // 成功的回调
