e71c40be887330171252f76156dd7bcbc00fb327
HYJ_coder <2628661255@qq.com>
2022-11-19 16:42:14 +0800

    封装登录的token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-
+import {login} from '@/api/user'
 const state = {
   token: getToken() //设置token ,初始化vuex的时候，从缓存中读取token
 }
@@ -13,7 +13,23 @@ const mutations = {
     removeToken()//将token同步给缓存
   }
 }
-const actions = {}
+const actions = {
+  //调用登录接口
+  async login(context,data){
+    // //调用接口
+    // const res = await login(data)
+    // //axios默认加了一层data
+    // if (res.data.success) {
+    //   // success为true表示登录成功
+    //   context.commit('setToken', res.data.data)
+    // }
+    login(data).then(res=>{
+      if (res.data.success) {
+        context.commit('setToken', res.data.data)
+      }
+    })
+  }
+}
 export default {
   namespaced: true,
   state,
