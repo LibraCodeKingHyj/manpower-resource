e4d6c0e9f60c0821d18ef29f3df8bfb457af67cc
HYJ_coder <2628661255@qq.com>
2022-11-27 15:51:41 +0800

    员工页面删除功能完成与新增页面铺设完成

--- src/api/employees.js
+++ src/api/employees.js
@@ -5,9 +5,27 @@ export function getEmployeeSimple() {
     url: '/sys/user/simple'
   })
 }
+/* 获取员工列表 */
 export function getEmployeeList(params) {
   return request({
     url: '/sys/user',
     params
+  })
+}
+/* 删除功能实现 */
+export function delEmployee(id) {
+  return request({
+    url: `/sys/user/${id}`,
+    method: 'delete'
+  })
+}
+/** **
+ *  新增员工的接口
+ * **/
+export function addEmployee(data) {
+  return request({
+    method: 'post',
+    url: '/sys/user',
+    data
   })
 }
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,7 @@
         <template #after>
           <el-button size="small" type="success">excel导入</el-button>
           <el-button size="small" type="danger">excel导出</el-button>
-          <el-button size="small" type="primary">新增员工</el-button>
+          <el-button size="small" type="primary" @click="showDialog=true">新增员工</el-button>
         </template>
       </page-tools>
       <el-card v-loading="loading">
@@ -19,32 +19,44 @@
           <el-table-column label="序号" sortable="" type="index" />
           <el-table-column label="姓名" sortable="" prop="username" />
           <el-table-column label="工号" sortable="" prop="workNumber" />
-          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" :formatter="formatEmployment" />
+          <el-table-column
+            label="聘用形式"
+            sortable=""
+            prop="formOfEmployment"
+            :formatter="formatEmployment"
+          />
           <el-table-column label="部门" sortable="" prop="departmentName" />
           <el-table-column label="入职时间" sortable="" prop="timeOfEntry">
             <template v-slot="obj">
-              {{
-                obj.row.timeOfEntry | formatDate
-              }}
+              {{ obj.row.timeOfEntry | formatDate }}
             </template>
           </el-table-column>
           <el-table-column label="账户状态" sortable="" prop="enableState">
-            <template v-slot="{row}">
-              <el-switch :value="row.enableState===1" />
+            <template v-slot="{ row }">
+              <el-switch :value="row.enableState === 1" />
             </template>
           </el-table-column>
           <el-table-column label="操作" sortable="" fixed="right" width="280">
-            <template>
+            <template v-slot="{ row }">
               <el-button type="text" size="small">查看</el-button>
               <el-button type="text" size="small">转正</el-button>
               <el-button type="text" size="small">调岗</el-button>
               <el-button type="text" size="small">离职</el-button>
               <el-button type="text" size="small">角色</el-button>
-              <el-button type="text" size="small">删除</el-button>
+              <el-button
+                type="text"
+                size="small"
+                @click="delEmployee(row.id)"
+              >删除</el-button>
             </template>
           </el-table-column>
         </el-table>
-        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+        <el-row
+          type="flex"
+          justify="center"
+          align="middle"
+          style="height: 60px"
+        >
           <el-pagination
             layout="prev, pager, next"
             :page-size="page.size"
@@ -55,13 +67,18 @@
         </el-row>
       </el-card>
     </div>
+    <AddEmployee :show-dialog.sync="showDialog" />
   </div>
 </template>
 
 <script>
-import { getEmployeeList } from '@/api/employees'
+import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees'
+import AddEmployee from './components/addEmployee.vue'
 export default {
+  components: {
+    AddEmployee
+  },
   data() {
     return {
       loading: false,
@@ -70,7 +87,8 @@ export default {
         page: 1, // 当前页码
         size: 10,
         total: 0 // 总数
-      }
+      },
+      showDialog: false
     }
   },
   created() {
@@ -99,13 +117,23 @@ export default {
         cellValue 每一行对应的单元格的值
         index:索引
       */
-      const obj = EmployeeEnum.hireType.find(item => item.id === cellValue)
+      const obj = EmployeeEnum.hireType.find((item) => item.id === cellValue)
       return obj ? obj.value : '小黑子'
+    },
+    // 删除
+    async delEmployee(id) {
+      try {
+        await this.$confirm('确定删除该员工吗？')
+        await delEmployee(id)
+        this.$message.success('删除员工成功')
+        this.getEmployeeList()
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
 </script>
 
 <style>
-
 </style>
